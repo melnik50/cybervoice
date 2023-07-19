a9db1ee791358802020a91ef4feb9201c5c41c7e
Nikita <nikita@trbyte.ru>
2023-07-19 22:08:37 +0300

    1

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
   const ws = useRef(null);
   const hash = useRef(null);
   const chatId = useRef("");
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
   const [isNewMessage, setNewMessage] = useState(false);
   const [isFirst, setIsFirst] = useState(true);
   const [isFormActive, setFormIsActive] = useState(true);
@@ -41,7 +43,6 @@ function App() {
   }, [isNewMessage]);
 
   const hideForm = () => {
-    console.log(1);
     setFormIsActive(false);
   }
 
@@ -56,7 +57,7 @@ function App() {
         chatId.current = message["chat_id"];
         hash.current = message["hash"];
         if(isFirst) {
-          renderCustomComponent(ClientForm, {hash: hash.current, chat_id: chatId.current, hideForm: hideForm});
+          renderCustomComponent(ClientForm, {hash: hash.current, chat_id: chatId.current, setName: setName, setPhone: setPhone, hideForm: hideForm});
         }
       }
 
@@ -72,14 +73,18 @@ function App() {
     messages.set('chat_id', chatId.current);
     messages.set('type', 'request');
     messages.set('is_first', 'false');
+    messages.set('name', '');
+    messages.set('phone', '');
 
     if(isFirst) {
       setIsFirst(false);
       messages.set('is_first', 'true');
+      messages.set('name', name);
+      messages.set('phone', phone);
     }
 
     ws.current.send(JSON.stringify(Object.fromEntries(messages)));
-
+    
     setNewMessage(!isNewMessage);
   };
 
--- src/components/clientForm.jsx
+++ src/components/clientForm.jsx
@@ -2,39 +2,8 @@ import React from 'react'
 import { useState } from 'react'
 
 function ClientForm(props) {
-    const [name, setName] = useState("");
-    const [phone, setPhone] = useState("");
-    
-    async function sendForm() {
-        try {
-            const res = await fetch(`https://clients.trbyte.ru/chatForm.php`, {
-                headers: { 'Content-Type': 'application/json' },
-                method: "POST",
-                body: JSON.stringify(
-                    {   
-                        name: name, 
-                        phone: phone,
-                        text: "",
-                        hash: props.hash,
-                        chat_id: "",
-                        type: 'request',
-                        is_first: 'false',
-                    }
-                ),
-            });
-            const data = await res.json();
-            console.log(data);
-            if (!res.ok) {
-                throw new Error(data);
-            }
-            props.hideForm(false);
-        } catch (e) {
-            return e;
-        }
-    }
-
     const closeForm = () => {
-        props.hideForm(false);
+        props.hideForm();
     }
 
     return (
@@ -42,18 +11,18 @@ function ClientForm(props) {
             <p>Представьтесь</p>
             <label>
                 <span>Имя:</span>
-                <input name="Имя" type="text" onChange={e => setName(e.target.value)}/>
+                <input name="Имя" type="text" onChange={e => props.setName(e.target.value)}/>
             </label>
             <label>
                 <span>Телефон:</span>
                 <input
                     name="phone" 
                     type="phone"
-                    onChange={e => setPhone(e.target.value)}
+                    onChange={e => props.setPhone(e.target.value)}
                 />
             </label>
             <div className="px-client-form-buttons">
-                <div className="px-client-form-save button" onClick={sendForm}>Сохранить</div>
+                <div className="px-client-form-save button" onClick={closeForm}>Сохранить</div>
                 <div className="px-client-form-close button b-transparent" onClick={closeForm}>Закрыть</div>
             </div>
         </form>
